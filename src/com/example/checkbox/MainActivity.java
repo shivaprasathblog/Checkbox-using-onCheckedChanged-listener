24c4e0b1cfb36ef5402b207c5f7849a1a79d9cef
Shivaprasath <shivaprasathblog@users.noreply.github.com>
2015-11-16 13:53:51 +0530

    Update MainActivity.java

--- src/com/example/checkbox/MainActivity.java
+++ src/com/example/checkbox/MainActivity.java
@@ -10,20 +10,20 @@ import android.app.Activity;
 
 public class MainActivity extends Activity
 {
-	//Define Checkbox as c1,c2,c3 respectively
+	//STEP 1: Declare checkbox as c1,c2,c3; respectively
 	CheckBox c1,c2,c3;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		//Refer the checkbox with their respective id's from XML
+		 
+		//STEP 2: Refer the checkboxs from xml
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		c1=(CheckBox)findViewById(R.id.checkBox1);
 		c2=(CheckBox)findViewById(R.id.checkBox2);
 		c3=(CheckBox)findViewById(R.id.checkBox3);
 
-	//Set the listener for Checkbox, we use onclik listener here 
+		//STEP 3: Set the listener & create the object for listener,checkbox c1
 		c1.setOnCheckedChangeListener(new OnCheckedChangeListener()
 		{
 			/**public abstract void onCheckedChanged (CompoundButton buttonView, boolean isChecked)
@@ -39,20 +39,23 @@ public class MainActivity extends Activity
 			public void onCheckedChanged(CompoundButton arg0, boolean arg1)
 			{
 				// TODO Auto-generated method stub
+				//STEP 3.1: Set the condition for checkbox c1
 				if(arg1==true)
 				{
-					//Toast message if c1 is checked(clicked)
+					//STEP 3.2: Set toast message for checkbox c1 if it is checked
 					Toast.makeText(MainActivity.this,"Salary Account is Selected",Toast.LENGTH_LONG).show();
 				}
+			
 				else if(arg1==false)
 				{
-					//Toast message if c1 is unchecked
+					//STEP 3.3: Set toast message for checkbox c1 if it is unchecked
 					Toast.makeText(MainActivity.this,"Salary Account is UnSelected",Toast.LENGTH_LONG).show();
 				}
 				
 			}
 		});
 		
+		//STEP 4: Set the listener & Create the object for listener,checkbox c2
 		c2.setOnCheckedChangeListener(new OnCheckedChangeListener() 
 		{
 			
@@ -60,19 +63,21 @@ public class MainActivity extends Activity
 			public void onCheckedChanged(CompoundButton arg0, boolean arg1) 
 			{
 				// TODO Auto-generated method stub
+				//STEP 4.1: Set the condition for c2 checkbox
 				if(arg1==true)
 				{
-					//Toast message if c2 is checked(clicked)					
+					//STEP 4.2: Set toast message for checkbox c2 if it is checked					
 					Toast.makeText(MainActivity.this,"Savings Account is Selected",Toast.LENGTH_LONG).show();
 				}
 				else if(arg1==false)
 				{
-					//Toast message if c2 is unchecked
+					//STEP 4.3: Set toast message for checkbox c2 if it is unchecked
 					Toast.makeText(MainActivity.this,"Savings Account is UnSelected",Toast.LENGTH_LONG).show();
 				}
 			}
 		});
 		
+		//STEP 5: Set the listener & Create the object for listener,checkbox c3
 		c3.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			
 			@Override
@@ -81,12 +86,12 @@ public class MainActivity extends Activity
 				// TODO Auto-generated method stub
 				if(arg1==true)
 				{
-					//Toast message if c3 is checked(clicked)	
+					//STEP 5.1: Set the condition for checkbox c3	
 					Toast.makeText(MainActivity.this,"Current Account is Selected",Toast.LENGTH_LONG).show();
 				}
 				else if(arg1==false)
 				{
-					//Toast message if c3 is unchecked
+					//STEP 5.2: Set toast message for c3 checkbox if it is checked
 					Toast.makeText(MainActivity.this,"Current Account is UnSelected",Toast.LENGTH_LONG).show();
 				}
 				
